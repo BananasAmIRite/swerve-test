04a0a10936a52d329fede7960dabc8ad92953d59
mattperls-code <mattperls.code@gmail.com>
2023-05-23 14:22:15 -0400

    fixed squaring signage loss, set up to test out corrective strafing

--- src/main/java/frc/robot/subsystems/drivetrain/commands/CorrectiveStrafingTeleopDrive.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/CorrectiveStrafingTeleopDrive.java
@@ -1,9 +1,11 @@
 package frc.robot.subsystems.drivetrain.commands;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.util.ControllerUtils;
 import frc.robot.util.DriverController;
 
 public class CorrectiveStrafingTeleopDrive extends CommandBase {
@@ -20,28 +22,44 @@ public class CorrectiveStrafingTeleopDrive extends CommandBase {
     @Override
     public void execute(){
         // get inputs, scale appropriately, calculate desired speed
-        double inputOmega = -Math.pow(this.controller.getRightStickX(), 2) * this.controller.getMaxRotation();
-        double inputStrafeX = -Math.pow(this.controller.getLeftStickX(), 2) * this.controller.getMaxSpeed();
-        double inputStrafeZ = -Math.pow(this.controller.getLeftStickY(), 2) * this.controller.getMaxSpeed();
+        double inputOmega = -ControllerUtils.squareKeepSign(this.controller.getRightStickX()) * this.controller.getMaxRotation();
+        double inputStrafeX = -ControllerUtils.squareKeepSign(this.controller.getLeftStickX()) * this.controller.getMaxSpeed();
+        double inputStrafeZ = -ControllerUtils.squareKeepSign(this.controller.getLeftStickY()) * this.controller.getMaxSpeed();
         double inputStrafeSpeed = Math.sqrt(inputStrafeX * inputStrafeX + inputStrafeZ * inputStrafeZ);
+
+        // ! uncomment after testing
+        // if(inputOmega == 0 || inputStrafeSpeed == 0){
+        //     this.drivetrain.swerveDrive(new ChassisSpeeds(inputStrafeZ, inputStrafeX, inputOmega));
+        //     return;
+        // }
 
         // calculate the angle of input strafe, rotate 90 degrees in opposite direction of turn
         double inputStrafeAngle = Math.atan2(inputStrafeZ, inputStrafeX);
         // TODO: plus or minus 90 degrees?
         double correctiveStrafeAngle = inputStrafeAngle - 0.5 * Math.PI * Math.signum(inputOmega);
 
-        // corrective strafe speed should be proportional to both the desired strafe speed as well as the desired angular speed
-        double correctiveStrafeSpeed = Constants.DrivetrainConstants.kCorrectiveStrafe_P * inputStrafeSpeed * Math.abs(inputOmega);
-        double correctiveStrafeX = correctiveStrafeSpeed * Math.cos(correctiveStrafeAngle);
-        double correctiveStrafeZ = correctiveStrafeSpeed * Math.sin(correctiveStrafeAngle);
+        SmartDashboard.putNumber("input-strafe-angle", inputStrafeAngle);
+        SmartDashboard.putNumber("corrective-strafe-angle", correctiveStrafeAngle);
 
-        // add corrective strafe to input
-        double netStrafeX = inputStrafeX + correctiveStrafeX;
-        double netStrafeZ = inputStrafeZ + correctiveStrafeZ;
+        // ! testing
+        this.drivetrain.swerveDrive(new ChassisSpeeds(inputStrafeZ, inputStrafeX, inputOmega));
+        return;
 
-        ChassisSpeeds correctedSpeeds = new ChassisSpeeds(netStrafeZ, netStrafeX, inputOmega);
+        // ! uncomment after testing
+        /*
+            // corrective strafe speed should be proportional to both the desired strafe speed as well as the desired angular speed
+            double correctiveStrafeSpeed = Constants.DrivetrainConstants.kCorrectiveStrafe_P * inputStrafeSpeed * Math.abs(inputOmega);
+            double correctiveStrafeX = correctiveStrafeSpeed * Math.cos(correctiveStrafeAngle);
+            double correctiveStrafeZ = correctiveStrafeSpeed * Math.sin(correctiveStrafeAngle);
 
-        this.drivetrain.swerveDrive(correctedSpeeds);
+            // add corrective strafe to input
+            double netStrafeX = inputStrafeX + correctiveStrafeX;
+            double netStrafeZ = inputStrafeZ + correctiveStrafeZ;
+
+            ChassisSpeeds correctedSpeeds = new ChassisSpeeds(netStrafeZ, netStrafeX, inputOmega);
+
+            this.drivetrain.swerveDrive(correctedSpeeds);
+        */
     }
 
     @Override
